4890fa11851c0854d5c17e307895999337e499bb
Narveen K A <narveen@techversantinfotech.com>
2023-06-08 18:56:35 +0530

    commit

--- Routing/js/index.js
+++ Routing/js/index.js
@@ -1,33 +1,20 @@
-const express = require("express")
-const path = require('path')
-const app = express()
-   
-var PORT = process.env.port || 3000
-
-app.set("views", path.join(__dirname))
-app.set("view engine", "ejs")
+const express = require("express");
+const app = express();
+var PORT = 3000;
   
 app.get("/user", function(req, res){
   
     var name = req.query.name
     var age = req.query.age
       
-    console.log("Name :", name)
-    console.log("Age :", age)
-})
+    console.log("Name : ", name)
+    console.log("Age : ", age)
 
-app.get('/', (req, res) => {
-    res.json('<h1>Home page</h1>');
-});
+    res.json(req.query)
+})
     
 app.get('/:id', (req, res) => {
-    const data = req.params.id;
-
-    console.log("Name: ", data.name);
-    console.log("Age: ", data.age);
-    console.log("Gender: ", data.gender);
-    
-    res.json(data);
+    res.json(req.params)
 });
 
 app.use(express.json());
@@ -45,6 +32,6 @@ app.post('/signup', function (req, res) {
 });
 
 app.listen(PORT, function(error){
-    if(error) throw error
-    console.log("Server created Successfully on PORT", PORT)
-})
+    if(error) throw error;
+    console.log("Server created Successfully on PORT", PORT);
+});
